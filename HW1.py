3272db7a26276177b16c9eefdba5417a45772f0f
mgbrwan <118233737+mgbrwan@users.noreply.github.com>
2023-09-06 23:19:19 +0800

    Update HW1.py

--- HW1.py
+++ HW1.py
@@ -1,8 +1,8 @@
 #%%
 height = float(input("請輸入您的身高(公尺):"))
-Weight = float(input("請輸入您的體重(公斤):"))
+weight = float(input("請輸入您的體重(公斤):"))
 
-BMI = Weight/(height**2)
+BMI = weight/(height**2)
 
 print(f"您的BMI是:{BMI:.2f}")
 #.2是指定浮點數到小數點第二位
