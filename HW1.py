032301c2c3c6bae7b585a655c5e99ff5e5096dfe
mgbrwan <118233737+mgbrwan@users.noreply.github.com>
2023-09-06 15:57:52 +0800

    Add files via upload
    python course week 1

---
+++ HW1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#%%
+height = float(input("請輸入您的身高(公尺):"))
+Weight = float(input("請輸入您的體重(公斤):"))
+
+BMI = Weight/(height**2)
+
+print(f"您的BMI是:{BMI:.2f}")
+
+#%%
