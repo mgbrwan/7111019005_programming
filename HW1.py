3fcb7880b8b65636ca9be632437caccdc4fb2262
mgbrwan <118233737+mgbrwan@users.noreply.github.com>
2023-09-06 23:16:06 +0800

    Update HW1.py

--- HW1.py
+++ HW1.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 #%%
 height = float(input("請輸入您的身高(公尺):"))
 Weight = float(input("請輸入您的體重(公斤):"))
@@ -11,5 +5,6 @@ Weight = float(input("請輸入您的體重(公斤):"))
 BMI = Weight/(height**2)
 
 print(f"您的BMI是:{BMI:.2f}")
+#.2是指定浮點數到小數點第二位
 
 #%%
